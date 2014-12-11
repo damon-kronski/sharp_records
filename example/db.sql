2837fcca1dae60bc02cb07a8ecf144742f97420e
damon-kronski <suter.melvin@gmail.com>
2014-12-11 12:03:42 +0100

    Update db.sql
    Change table name to correct one...

--- example/db.sql
+++ example/db.sql
@@ -1,4 +1,4 @@
-CREATE TABLE [dbo].[examplemodel] (
+CREATE TABLE [dbo].[ExampleItem] (
     [id]    INT           IDENTITY (1, 1) NOT NULL,
     [value] VARCHAR (255) DEFAULT ('') NULL,
     PRIMARY KEY CLUSTERED ([id] ASC)
